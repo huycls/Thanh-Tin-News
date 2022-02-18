49e9fecae05ae2695ec6321e59a6251795061ff5
hoang huy <hoanghuycls@gmail.com>
2022-02-18 11:29:03 +0700

    sua giao dien lan 2

--- frontend/src/components/ApplicationReview.js
+++ frontend/src/components/ApplicationReview.js
@@ -13,12 +13,14 @@ export default withNamespaces((props) => props.namespaces)(function ApplicationR
           dropdown1.classList.remove("show");
         }
       }
+    const formatter = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' });
     return (  
             <div key= {application._id}  className="newsreview-component">
                 <Link className="news-link-wrapper" onClick={closeDropdown} to={`/tin-moi/${application._id}`}>
                     <img className="news-link-img" src={application.articleimage} alt="news-img"></img>
                     <p className="news-link">
                         {t("title", {application}).length > 30 ? t("title",{application}).substring(0, 47) + "..." : t("title",{application})}
+                        <small>{formatter.format(new Date(application.createdAt))}</small>
                     </p>
                 </Link>
                 </div>
